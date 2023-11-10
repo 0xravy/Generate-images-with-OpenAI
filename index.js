57628c4f2d9f55ff2840bffa9e454803a7c36d52
aravns <pythonjavacppcode@gmail.com>
2023-11-10 22:16:37 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const api = "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";
+const inpt = document.getElementById("inpt");
+const images = document.getElementById("images");
+
+const getImages = async () => {
+    const methods = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${api}`,
+        },
+        body: JSON.stringify({
+            prompt: inpt.value,
+            n: 3,
+            size: "1024x1024",
+        }),
+    };
+
+    try {
+        const res = await fetch(
+            "https://api.openai.com/v1/images/generations",
+            methods
+        );
+
+        if (res.status === 429) {
+            console.error("Rate limit exceeded. Please wait and try again.");
+            return;
+        }
+
+        const myData = await res.json();
+
+        // التحقق من وجود imageList قبل استخدام map
+        const imageList = myData.data || [];
+
+        images.innerHTML = "";
+
+        // استخدام forEach بدلاً من map للتعامل مع قائمة فارغة بشكل أفضل
+        imageList.forEach((photo) => {
+            const div = document.createElement("div");
+            images.append(div);
+            const img = document.createElement("img");
+            div.append(img);
+            img.src = photo.url;
+        });
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+};
